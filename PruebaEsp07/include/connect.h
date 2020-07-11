772e127f49f6fe64d121193c8b2e1eaedbbd35b2
juankgp <jcfenix01@gmail.com>
2020-07-11 12:51:26 -0500

    esp07 completo falla ap

---
+++ PruebaEsp07/include/connect.h
@@ -0,0 +1,84 @@
+#include <Updater.h>
+#include <ESP8266mDNS.h>
+#include <Hash.h>
+#include "requests.h"
+char* ssid = "esp8285";
+char* password = "esp8285pass";
+String wifiData="";
+
+boolean wifiConnect(char* host,boolean caso, String ssidtxt ,String passtext) {
+//#ifdef MYSSID
+if(caso){
+  Serial.println("Conectandose a una red");
+  WiFi.mode(WIFI_AP_STA);
+    //while(WiFi.waitForConnectResult() != WL_CONNECTED){
+    for (int i = 0; i < 3; i++)
+    {
+      WiFi.begin(ssidtxt, passtext);
+      Serial.println("WiFi failed, retrying.");
+       if (WiFi.waitForConnectResult() == WL_CONNECTED) break;
+     
+    }
+    if (WiFi.waitForConnectResult() != WL_CONNECTED)
+    {
+      writetxt("");
+      Serial.println("No se pudo conectar");
+      ESP.restart();
+    }
+     else
+     {
+       WiFi.softAPdisconnect(true);
+      Serial.println(WiFi.localIP());
+   return (WiFi.status() == WL_CONNECTED);
+     }
+      
+}
+else{
+  Serial.println("Creando Acces Point");
+  WiFi.mode(WIFI_AP);
+  //WiFi.begin();
+  // WiFi.waitForConnectResult();
+  while(!WiFi.softAP(ssid, password))
+  {
+   Serial.println(".");
+    delay(1000);
+  }
+  Serial.print("Iniciado AP ");
+  Serial.println(ssid);
+  Serial.print("IP address:\t");
+  Serial.println(WiFi.softAPIP());
+  return WiFi.softAP(ssid, password);
+  }
+//#endif
+}
+
+void appendtxt(String datos){
+  myFile = SPIFFS.open(myFilePath, "a");
+  if(myFile.print(datos)){
+    Serial.println("Message successfully appended");  
+  }
+  else{
+    Serial.print("Appending failled!");  
+  }
+  myFile.close();
+}
+boolean readtxt(){
+  boolean haydata=false;
+  myFile = SPIFFS.open(myFilePath, "r");
+  wifiData="";
+  while(myFile.available()) {
+    //Serial.write(myFile.read());
+    wifiData += (char)myFile.read();
+  }
+  Serial.println("\"");   
+  //Serial.println("String completa" + wifiData);
+
+  // Check file size
+  Serial.print("File size: ");
+  Serial.println(myFile.size());
+  if(myFile.size()>=3) haydata = true;
+  else haydata = false;
+  myFile.close();
+  return  haydata;
+}
+
--- PruebaEsp07/src/main.cpp
+++ PruebaEsp07/src/main.cpp
@@ -1,9 +1,123 @@
 #include <Arduino.h>
+#include <FS.h>
+#include <TimeLib.h>
+#include <EEPROM.h>
+#include <TimeAlarms.h>
+#include <DS3232RTC.h>     
+#include <Streaming.h>      
+#include <Wire.h>
+#include "ota.h"
+
+int addr = 0;
+int totalHor = 0;
+int totalMin = 0;
+int totalSeg = 0;
+int totaltEspera = 0;
+
+int cambioluz=0;
+
 
 void setup() {
-  // put your setup code here, to run once:
+
+#pragma region //Configuracio
+  Serial.begin(115200);
+  pinMode(ozono,OUTPUT);//ozono
+  pinMode(humo,OUTPUT);
+  pinMode(luces,OUTPUT);
+  pinMode(vent1,OUTPUT);
+  pinMode(vent2,OUTPUT);
+  pinMode(vent3,OUTPUT);
+  pinMode(vent4,OUTPUT);
+ 
+  digitalWrite(humo,LOW);
+  digitalWrite(ozono,LOW);
+  digitalWrite(luces,LOW);
+  digitalWrite(vent1,LOW);
+  digitalWrite(vent2,LOW);
+  digitalWrite(vent3,LOW);
+  digitalWrite(vent4,LOW);
+  
+ 
+  EEPROM.begin(512);
+  char host[16];
+  Serial.println("Iniciando");
+
+  Wire.begin(9, 10);
+
+  //AsyncWiFiManager wifiManager(&server,&dns);
+  //wifiManager.resetSettings();
+ // wifiManager.autoConnect("testAPJK", "juank123");
+if(!SPIFFS.begin()){
+        Serial.println("An Error has occurred while mounting SPIFFS");
+        return;
+  }
+ //writetxt("FLA GUTIERREZ:KYARALIBY:");
+ //writetxt("");
+
+readtxt();
+
+  int sepssid = wifiData.indexOf(":");
+  int seppass = wifiData.indexOf(":",sepssid+1);
+  String ssidtext = wifiData.substring(0,sepssid);
+  String passtext = wifiData.substring(sepssid+1,seppass);
+  //Serial.println("datos" + ssidtext + "---" +passtext);
+  //ssidtext = "FLA GUTIERREZ";
+  //passtext = "KYARALIBY";
+
+
+ #ifdef ESP8266
+  snprintf(host, 12, "ESP%08X", ESP.getChipId());
+  #else
+  snprintf(host, 16, "ESP%012llX", ESP.getEfuseMac());
+  #endif
+
+
+
+   if(!wifiConnect(host,readtxt(),ssidtext,passtext)) {
+     Serial.println("Connection failed");
+     //digitalWrite(redLed,LOW);
+     return;
+   }
+   //digitalWrite(redLed,HIGH);
+  MDNS.begin(host);
+  if(webInit()) MDNS.addService("http", "tcp", 80);
+  /*  tmElements_t tm;
+     tm.Hour = 00;               // set the RTC to an arbitrary time
+     tm.Minute = 05;
+     tm.Second = 00;
+     tm.Day = 04;
+     tm.Month = 07;
+     tm.Year = 2020 - 1970;      // tmElements_t.Year is the offset from 1970
+     RTC.write(tm);*/     
+  setSyncProvider(RTC.get);
+	setSyncInterval(300);
+
+   if(timeStatus() != timeSet)
+        Serial.println("Unable to sync with the RTC");
+    else
+        Serial.println("RTC has set the system time");
+
+
+  #pragma endregion
+ 
+  requests();
+  //*****************************************************
+  Alarm.timerOnce(timeInit*multipliSeg, AlarmFunctionON);
+  alarmWork=Alarm.timerOnce(timeTrabajo*multipliSeg, AlarmDisableWork);
+  
+  /*digitalWrite(ozono,HIGH);
+  digitalWrite(vent1,HIGH);
+  digitalWrite(vent2,HIGH);
+  digitalWrite(vent3,HIGH);
+  digitalWrite(vent4,HIGH);*/
+  Serial.println("Alarmas creadas");
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop(){
+
+  Alarm.delay(1000);
+  
+  Serial.println(String(hour()) + ":" + String(minute()) + ":" +String(second()));
+
 }
+
